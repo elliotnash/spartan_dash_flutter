470a53c881d077e8109d3d75a27b785c2c8dadd5
Elliot Nash <elliotnash@gmail.com>
2023-04-17 10:01:04 -0700

    Add grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:cbor/simple.dart';
+import 'dart:math';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart';
+import 'package:spannable_grid/spannable_grid.dart';
 import 'package:spartan_dash_flutter/models/dash_layout.dart';
 import 'package:spartan_dash_flutter/models/widgets.dart';
 import 'package:system_theme/system_theme.dart';
@@ -72,26 +74,108 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  final layout = const DashLayout(
+    columns: 3,
+    rows: 2,
+    widgets: [
+      WidgetPlacement(
+        widgetUuid: "0",
+        column: 0,
+        row: 0,
+        columnSpan: 1,
+        rowSpan: 1,
+      ),
+      WidgetPlacement(
+        widgetUuid: "1",
+        column: 2,
+        row: 1,
+        columnSpan: 1,
+        rowSpan: 1,
+      ),
+    ],
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final widgets = const [
+    SelectorData(
+      name: "Drive Mode",
+      uuid: "0",
+      options: {
+        "arcade": "Arcade",
+        "curvature": "Curvature"
+      },
+      selected: "arcade",
+    ),
+    ToggleData(
+        name: "Robot Working",
+        uuid: "1",
+        toggleType: ToggleType.slider,
+        enabled: false
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FilledButton(
-        onPressed: () {},
-        child: const Text("HI"),
-      )
+    // return Center(
+    //   child: FilledButton(
+    //     onPressed: () {},
+    //     child: const Text("HI"),
+    //   )
+    // );
+    const double gridPadding = 4;
+    return Padding(
+      padding: const EdgeInsets.all(gridPadding),
+      child: SpannableGrid(
+        columns: layout.columns,
+        rows: layout.rows,
+        editingStrategy: SpannableGridEditingStrategy.disabled(),
+        style: const SpannableGridStyle(
+          backgroundColor: Colors.transparent,
+          contentOpacity: 1,
+          spacing: gridPadding,
+        ),
+        cells: layout.widgets.where((e) => e.column >= 0
+            && e.column < layout.columns
+            && e.row >= 0
+            && e.row < layout.rows
+        ).map((e) {
+          // Clamp the span to within the grid.
+          final columnEnd = (e.column + e.columnSpan).clamp(0, layout.columns);
+          final rowEnd = (e.row + e.rowSpan).clamp(0, layout.rows);
+          final columnSpan = columnEnd - e.column;
+          final rowSpan = rowEnd - e.row;
+
+          return SpannableGridCellData(
+            id: e,
+            column: e.column+1,
+            row: e.row+1,
+            columnSpan: columnSpan,
+            rowSpan: rowSpan,
+            child: Container(
+              color: Colors.green,
+            ),
+          );
+        }).toList(),
+        //  [
+        //   SpannableGridCellData(
+        //     id: "0",
+        //     column: 1,
+        //     row: 1,
+        //     columnSpan: 2,
+        //     rowSpan: 2,
+        //     child: Container(
+        //       color: Colors.green,
+        //     ),
+        //   ),
+        //   SpannableGridCellData(
+        //     id: "1",
+        //     column: 4,
+        //     row: 3,
+        //     child: Container(
+        //       color: Colors.red,
+        //     ),
+        //   ),
+        // ]
+      ),
     );
   }
 }
